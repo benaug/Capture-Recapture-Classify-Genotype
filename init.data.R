d1ca4410cf3fe1cb9e9c03cf35945d2ea4f58ca8
Ben Augustine <ba378@cornell.edu>
2024-04-07 17:17:14 -0400

    initial commit

---
+++ init.data.R
@@ -0,0 +1,85 @@
+init.data <- function(data=NA,M=NA,inits=inits,initTrue=FALSE){
+  this.k <- data$this.k
+  K <- data$K
+  n.cov <- data$n.cov
+  n.levels <- data$n.levels
+  IDcovs <- data$IDlist$IDcovs
+  
+  n.samples <- length(this.k)
+  G.obs <- data$G.obs
+  if(!is.array(G.obs))stop("G.obs must be an array")
+  n.rep <- dim(G.obs)[3]
+  n.cov <- dim(G.obs)[2]
+  
+  ##pull out initial values for gamma
+  gammaMat <- inits$gammaMat
+  
+  #initialize G.obs.true, the true sample-level full categorical identities
+  #initializing to the most commonly observed sample by category values across observers
+  G.obs.true <- matrix(0,nrow=n.samples,ncol=n.cov)
+  for(i in 1:n.samples){
+    for(l in 1:n.cov){
+      vec <- G.obs[i,l,]
+      if(any(is.na(vec))){
+        vec <- vec[-which(is.na(vec))]
+      }
+      if(length(vec)>0){
+        choose <- as.integer(names(which(table(vec)==max(table(vec)))))
+        if(length(choose)>1){
+          pick <- sample(1:length(choose),1)
+          choose <- choose[pick]
+        }
+        G.obs.true[i,l] <- choose
+      }
+    }
+  }
+
+  y.true <- matrix(0,M,K)
+  if(initTrue==TRUE){
+    ID <- data$ID
+  }else{
+    ID <- sample(1:M,n.samples,replace=TRUE)
+  }
+  for(l in 1:n.samples){
+    y.true[ID[l],this.k[l]] <- y.true[ID[l],this.k[l]] + 1
+  }
+  
+  #Initialize z
+  z <- 1*(apply(y.true,1,sum)>0)
+  N <- sum(z)
+  
+  #Initialize G.true
+  G.true <- matrix(0, nrow=M,ncol=n.cov)
+  for(i in 1:max(ID)){
+    idx <- which(ID==i)
+    if(length(idx)==1){
+      G.true[i,] <- G.obs.true[idx,]
+    }else if(length(idx)>1){
+      if(ncol(G.obs.true)>1){
+        G.true[i,] <- apply(G.obs.true[idx,],2, max) #consensus
+      }else{
+        G.true[i,] <- max(G.obs.true[idx,])
+      }
+    }#else need to fill below
+  }
+  #Fill in missing values, create indicator for them
+  G.latent <- G.true==0
+  for(j in 1:n.cov){
+    fix <- G.true[,j]==0
+    G.true[fix,j] <- sample(IDcovs[[j]],sum(fix),replace=TRUE,prob=gammaMat[j,1:n.levels[j]])
+  }
+  
+  
+  #Converting NAs in G.obs to some other arbitrary value that is not a genotype number to avoid NA warnings from nimble
+  G.obs <- data$G.obs
+  G.obs.NA.indicator <- is.na(G.obs)
+  G.obs[is.na(G.obs)] <- 9999999
+  if(n.rep==1){
+    stop("n.rep==1 not currently handled")
+  }
+  if(n.cov==1){
+    stop("n.cov==1 not currently handled")
+  }
+  return(list(y.true=y.true,z=z,N=N,G.true=G.true,ID=ID,n.samples=n.samples,this.k=this.k,
+              G.latent=G.latent,G.obs=G.obs,G.obs.NA.indicator=G.obs.NA.indicator))
+}
---
+++ sim.data.R
@@ -0,0 +1,153 @@
+sim.data <-
+  function(N=NA,p.y=NA,lambda.y=NA,K=NA,p.geno.het=NA,
+           p.geno.hom=NA,n.cov=NA,n.rep=NA,
+           pID=NA,gamma=NA,IDcovs=NA,ptype=NA){
+    #error checks
+    if(length(gamma)!=n.cov)stop("gamma must be of length n.cov")
+    for(l in 1:n.cov){
+      if(length(gamma[[l]])!=length(IDcovs[[l]]))stop("gamma[[l]] must have one element per element of IDcovs[[l]]")
+      if(sum(gamma[[l]])!=1)stop("gamma[[l]] must sum to 1")
+    }
+    if(sum(p.geno.hom)!=1)stop("p.geno.hom must sum to 1")
+    if(sum(p.geno.het)!=1)stop("p.geno.het must sum to 1")
+    if(length(p.geno.hom)!=2)stop("p.geno.hom must be of length 2")
+    if(length(p.geno.het)!=3)warning("p.geno.het must be of length 3 unless simulating SNPs")
+    
+    #simulate IDcovs
+    n.levels <- unlist(lapply(gamma,length))
+    G.true <- matrix(NA,nrow=N,ncol=n.cov) #all IDcovs in population.
+    for(i in 1:N){
+      for(j in 1:n.cov){
+        G.true[i,j] <- sample(IDcovs[[j]],1,prob=gamma[[j]])
+      }
+    }
+    library(VGAM)
+    #Capture individuals
+    y.det <- y.count <- matrix(0,N,K)
+    for(i in 1:N){
+      for(k in 1:K){
+        y.det[i,k] <- rbinom(1,1,p.y)
+        if(y.det[i,k]==1){
+          y.count[i,k] <- rzapois(1,lambda.y,pobs0=0)
+        }
+      }
+    }
+    
+    #discard uncaptured inds and aggregate true IDcovs for all samples, keeping track of where they came from with A matrix (used to put observed data back together)
+    caught <- which(apply(y.count,c(1),sum)>0)
+    y.true <- y.count
+    y <- y.count[caught,]
+    n <- length(caught)
+    n.samples <- sum(y)
+    G.true <- G.true[caught,]
+    if(n.cov==1){
+      G.true <- matrix(G.true,ncol=1)
+    }
+    G.cap <- matrix(NA,nrow=n.samples,ncol=n.cov)
+    # ID <- rep(1:nrow(y),times=rowSums(y))
+    
+    #disaggregate samples
+    ID <- this.k <- rep(NA,n.samples)
+    idx <- 1
+    for(i in 1:n){ #loop through inds (uncaptured already removed)
+      for(k in 1:K){ #then occasions
+        if(y[i,k]>0){ #is there at least one sample here?
+          for(l in 1:y[i,k]){ #then samples
+            ID[idx] <- i #ID numbers don't count uncaptured guys
+            this.k[idx] <- k
+            G.cap[idx,]=G.true[i,]
+            idx <- idx+1
+          }
+        }
+      }
+    }
+    
+    
+    
+    # idx <- 1
+    # A=array(0,dim=c(dim(y),n.samples))  #configuration matrix: indicator matrix for which individual i occassion j  trap k corresponds to sample l. used to convert corrupt IDcovs to corrupt capture history
+    # for(i in 1:length(caught)){ #loop through inds (uncaptured already removed)
+    #   for(j in 1:J){ #then traps
+    #     for(k in 1:K){ #then occasions
+    #       if(y[i,j,k]>0){ #is there at least one sample here?
+    #         for(l in 1:y[i,j,k]){ #then samples
+    #           G.cap[idx,]=G.true[i,]
+    #           A[i,j,k,idx]=1
+    #           idx=idx+1
+    #         }
+    #       }
+    #     }
+    #   }
+    # }
+    # ycap=aperm(apply(A,c(2,3,4),sum),c(3,1,2))
+    
+    #double check that observed data can reconstruct true data
+    y.obs <- matrix(0,max(ID),K)
+    for(l in 1:n.samples){
+      y.obs[ID[l],this.k[l]] <- y.obs[ID[l],this.k[l]] + 1
+    }
+    if(!all(y.obs==y)){
+      stop("Error in data simulator!")
+    }
+    theta=vector("list",n.cov)
+    for(m in 1:n.cov){
+      theta[[m]]=matrix(0,nrow=n.levels[m],ncol=n.levels[m])
+      for(l in 1:n.levels[m]){
+        if(!any(ptype[[m]][l,]==2)){#homozygote
+          theta[[m]][l,which(ptype[[m]][l,]==1)]=(p.geno.hom[1])
+          theta[[m]][l,which(ptype[[m]][l,]==3)]=(p.geno.hom[2])*(1/sum(ptype[[m]][l,]==3))
+        }else{
+          theta[[m]][l,which(ptype[[m]][l,]==1)]=(p.geno.het[1])
+          theta[[m]][l,which(ptype[[m]][l,]==2)]=(p.geno.het[2])*(1/sum(ptype[[m]][l,]==2))
+          theta[[m]][l,which(ptype[[m]][l,]==3)]=(p.geno.het[3])*(1/sum(ptype[[m]][l,]==3))
+        }
+      }
+    }
+    
+    #observation error
+    G.error=array(NA,dim=c(n.samples,n.cov,n.rep))
+    for(k in 1:n.rep){
+      G.error[,,k]=G.cap
+      for(l in 1:n.cov){
+        for(i in 1:n.samples){
+          if(rbinom(1,1,pID[l])==1){
+            for(j in 1:n.levels[l]){
+              if(G.cap[i,l]==j){
+                G.error[i,l,k]=sample(IDcovs[[l]],1,prob=theta[[l]][j,])
+              }
+            }
+          }else{
+            G.error[i,l,k]=NA
+          }
+        }
+      }
+    }
+    #find errors that occurred
+    G.Obstype=array(0,dim=c(n.samples,n.cov,n.rep))
+    for(k in 1:n.rep){
+      for(l in 1:n.cov){
+        for(i in 1:n.samples){
+          if(is.na(G.error[i,l,k]))next#is missing
+          if(G.error[i,l,k]==G.cap[i,l]){#is correct
+            G.Obstype[i,l,k]=1
+          }else{#is an error
+            G.Obstype[i,l,k]=ptype[[l]][G.error[i,l,k],G.cap[i,l]]
+          }
+        }
+      }
+    }
+    getmode = function(v) {
+      uniqv = unique(v)
+      uniqv[which.max(tabulate(match(v, uniqv)))]
+    }
+    #generate a crude consensus genotype
+    G.consensus=apply(G.error,c(1,2),function(x){getmode(x[x!=0])})
+    #how many of the crude consensus genotypes are corrupted?
+    corrupted=sum(apply(G.consensus==G.cap,1,function(x){any(x==FALSE)}),na.rm=TRUE)
+    
+    out=list(y=y,this.k=this.k,G.true=G.true,G.obs=G.error,n.cov=n.cov,n.levels=n.levels,
+             n.samples=length(this.k),IDlist=list(n.cov=n.cov,IDcovs=IDcovs,ptype=ptype),
+             ID=ID,K=K,n=nrow(y),corrupted=corrupted,G.Obstype=G.Obstype)
+    
+    return(out)
+  }
